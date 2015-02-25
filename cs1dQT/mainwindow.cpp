046ad7053adc661b524f40c08339924e7fe6e162
packerfan626 <dori_mouawad@hotmail.com>
2015-02-24 20:38:59 -0800

    Revert "Test"
    This reverts commit 71264a85415074ba7f35858377033a7289234079.

--- cs1dQT/mainwindow.cpp
+++ cs1dQT/mainwindow.cpp
@@ -1,7 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "account.h"
-#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,22 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-//LOGIN BUTTON
-void MainWindow::on_pushButton_clicked()
-{
-    Account login;
-    login.setModal(true);
-    login.exec();
-
-
-    //Calls login function
-    login.Login();
-
-}
-
-void MainWindow::on_pushButton_2_clicked()
-{
-
+    // Austin test
+    // this is more stuff and stuff to test this out
+    // sajdflkjaskdf test  gay
 }
